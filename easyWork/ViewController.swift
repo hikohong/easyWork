6f5a43c7d53f59899e3ef849904d42b8def1153e
hikohong <hikohong@gmail.com>
2014-11-16 01:17:34 +0800

    Initial Commit

---
+++ easyWork/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  easyWork
+//
+//  Created by hikohong on 2014/11/16.
+//  Copyright (c) 2014年 hikohong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
